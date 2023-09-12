cbbc5606b1263376866874da79f4d0f0d3c48825
ampck <a1j2m4@gmail.com>
2023-09-12 12:09:30 -0400

    completed nft contract and deployment scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,20 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'Dapp University'
-  const SYMBOL = 'DAPP'
-  const MAX_SUPPLY = '1000000'
+  const NAME = 'Dapp Punks'
+  const SYMBOL = 'DP'
+  const COST = ethers.utils.parseUnits('10', 'ether')
+  const MAX_SUPPLY = 25
+  const ALLOW_MINTING_ON = (Date.now() + 60000).toString().slice(0,10)
+  const BASE_URI = 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/'
+  
+  const NFT = await ethers.getContractFactory('NFT')
+  nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
 
-  // Deploy Token
-  const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
-
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}\n`)
+  await nft.deployed()
+  console.log(`NFT deployed to: ${nft.address}\n`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
